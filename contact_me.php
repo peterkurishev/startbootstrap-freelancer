ba9a5e1f5ffe4a623bf6ba2ff7734fc8eb6435df
Peter A. Kurishev <peter.kurishev@gmail.com>
2020-01-12 12:14:40 +0300

    Mailing script config

--- contact_me.php
+++ contact_me.php
@@ -7,9 +7,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'peter.kurishev@gmail.com';
 $email_subject = "Новое обращение с сайта dswz.ru от  $name";
-$email_body = "Hola Verónica, recibiste un mensaje desde la planilla de contacto.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmpresa: $company\n\nEmail: $email_address\n\nTeléfono: $phone\n\nPaís: $country\n\nConsulta:\n$message";
-$headers = "From: noreply@trabajosaludable.com.ar\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$email_body = "Новый потенциальный клиент.\n\n"."Имя: $name\n\nEmail: $email_address\n\nНомер телефона: $phone\n\nСообщение:\n$message";
+$headers = "From: noreply@dswz.ru\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
 $result = mail($to,$email_subject,$email_body,$headers);
 die($result);
 ?>
